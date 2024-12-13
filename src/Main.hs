e39253ee5d694c8432e495bdff1b5e76665de6d2
ta4ilka69 <artem_balin_2004@mail.ru>
2024-12-13 04:39:58 +0300

    fixed china symbols (CRLF -> LF)

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,19 @@
 module Main where
 
+import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TIO
 import Markdown
 import Parser
 import System.Environment (getArgs)
-import System.IO (hSetEncoding, stdout, utf8)
+import System.IO (hSetEncoding, stdin, stdout, utf8)
 
 main :: IO ()
 main = do
   -- Set UTF-8 encoding for stdout
   hSetEncoding stdout utf8
-
+  hSetEncoding stdin utf8
   args <- getArgs
   case args of
     ["-c"] -> do
@@ -20,7 +21,8 @@ main = do
       let html = T.concat [T.pack "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n</head>\n<body>\n", markdownToHtml content, T.pack "\n</body>\n</html>"]
       TIO.putStrLn html
     [input, output] -> do
-      content <- TIO.readFile input
+      sinput <- BS.readFile input -- read raw bytes
+      let content = TE.decodeUtf8 sinput -- decode to UTF-8 Text
       let html = T.concat [T.pack "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n</head>\n<body>\n", markdownToHtml content, T.pack "\n</body>\n</html>"]
-      TIO.writeFile output html
+      BS.writeFile output (TE.encodeUtf8 html)
     _ -> putStrLn "Usage: program <input> <output> OR program -c"
