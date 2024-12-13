daf3d694ae1635ead5037800f1a5bedbfab307eb
ta4ilka69 <artem_balin_2004@mail.ru>
2024-12-13 04:29:36 +0300

    render with china chao nima

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,26 @@
 module Main where
 
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TIO
 import Markdown
 import Parser
 import System.Environment (getArgs)
+import System.IO (hSetEncoding, stdout, utf8)
 
 main :: IO ()
 main = do
+  -- Set UTF-8 encoding for stdout
+  hSetEncoding stdout utf8
+
   args <- getArgs
   case args of
     ["-c"] -> do
       content <- TIO.getContents
-      let html = markdownToHtml content
+      let html = T.concat [T.pack "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n</head>\n<body>\n", markdownToHtml content, T.pack "\n</body>\n</html>"]
       TIO.putStrLn html
     [input, output] -> do
       content <- TIO.readFile input
-      let html = markdownToHtml content
+      let html = T.concat [T.pack "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"UTF-8\">\n</head>\n<body>\n", markdownToHtml content, T.pack "\n</body>\n</html>"]
       TIO.writeFile output html
     _ -> putStrLn "Usage: program <input> <output> OR program -c"
